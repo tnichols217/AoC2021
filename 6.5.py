b1ec92873924aa923878b7c707699bc927e80229
tnichols <tnichols@student.dalat.org>
2021-12-06 14:51:27 +0800

    make 6.5 prettier

--- 6.5.py
+++ 6.5.py
@@ -1,10 +1,7 @@
 import numpy as np
 
 a = np.array(list(map(lambda a : int(a) ,open("6.txt", 'r').read().split(','))))
-b = np.array([0] * 9)
-
-for i in range(7):
-    b[i] = sum(a == i)
+b = [sum(a == i) for i in range(9)]
 
 for i in range(256):
     b = np.append(b[1:], b[0])
