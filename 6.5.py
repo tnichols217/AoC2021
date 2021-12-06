570ca7119525504cf6ff528f0af51e57ec2d9119
tnichols <tnichols@student.dalat.org>
2021-12-06 15:10:06 +0800

    make 6.5 cooler

--- 6.5.py
+++ 6.5.py
@@ -3,8 +3,7 @@ import numpy as np
 a = np.array(list(map(lambda a : int(a) ,open("6.txt", 'r').read().split(','))))
 b = [sum(a == i) for i in range(9)]
 
-for i in range(256):
-    b = np.append(b[1:], b[0])
-    b[6] += b[8]
+for i in range(256):    
+    b[(i + 7) % 9] += b[i % 9]
 
 print(sum(b))
